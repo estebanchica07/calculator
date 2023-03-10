6961af2dd48c53e02d02e9db888428af60253e83
estebanchica07 <estebanchica07@gmail.com>
2023-03-10 17:13:48 -0500

    Sumas dos numeros con inputs e imprimirlos

--- script.js
+++ script.js
@@ -1,41 +1,12 @@
-const hn = document.querySelector("h1");
-const pr = document.querySelector("p");
-const parraf = document.querySelector(".parrafito");
-const pi = document.getElementById("pid");
-const entrada = document.querySelector("input");
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.getElementById("botonCalcular");
+const pResult = document.getElementById("result");
 
-console.log(entrada.value);
-
-console.log({
-  hn,
-  pr,
-  parraf,
-  pi,
-  entrada,
-});
-
-hn.innerHTML = "Cambio <br> Otro cambio";
-pr.innerText = "Cambio pero en el texto <br> incluso con br";
-hn.setAttribute("class", "Pacilia");
-hn.classList.add("Cosmos");
-console.log(hn.getAttribute("class")); //Para imprimir el valor del atributo marca del elemento hn.
-hn.classList.remove("Pacilia");
-console.log(hn.getAttribute("class"));
-hn.classList.toggle("Terracota");
-console.log(hn.getAttribute("class"));
-hn.classList.toggle("Terracota");
-console.log(hn.getAttribute("class"));
-console.log(hn.classList.contains("Jorvas"));
-console.log(hn.getAttribute("class"));
-
-entrada.value = 123;
-document.createElement("span");
-
-const imagenNueva = document.createElement("img");
-imagenNueva.setAttribute(
-  "src",
-  "https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg"
-);
-pi.append(imagenNueva);
-parraf.innerHTML = "";
-parraf.appendChild(imagenNueva);
+function botonOnClick() {
+  const suma = Number(input1.value) + Number(input2.value);
+  pResult.append(`El resultado es ${suma}`);
+  debugger;
+  pResult.innerText = "Hola";
+}
