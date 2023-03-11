1120933e3e16c9245b0a3dd3e67555136a08ed3e
estebanchica07 <estebanchica07@gmail.com>
2023-03-10 22:08:59 -0500

    Calculadora programada para sumar en CSS

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const h1 = document.querySelector("h1");
+const form = document.querySelector("#formulario");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
-const btn = document.getElementById("botonCalcular");
+const btnCalcular = document.getElementById("botonCalcular");
+const btnReiniciar = document.getElementById("botonLimpiar");
 const pResult = document.getElementById("result");
 
-function botonOnClick() {
-  const suma = +input1.value + +input2.value;
-  pResult.append(`El resultado es ${suma}`);
+btnCalcular.addEventListener("click", sumarInputs);
+
+btnReiniciar.addEventListener("click", botonReiniciar);
+
+function sumarInputs() {
+  if (input1.value !== "" && input2.value !== "") {
+    pResult.innerText = "";
+    const suma = +input1.value + +input2.value;
+    pResult.append(`= ${suma}`);
+    pResult.style.color = "white";
+  } else {
+    pResult.innerText = "Enter two numeric values";
+    pResult.style.fontSize = "20px";
+  }
 }
 
 function botonReiniciar() {
   input1.value = "";
   input2.value = "";
-  pResult.innerText = "";
+  pResult.innerText = "Result";
+  pResult.style.color = "#A2AABF";
 }
